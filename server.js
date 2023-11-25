07b6e1361b46d54a6f2f0b9c6aacd8e419a9fa47
unknown <16635847+Areeb2016@users.noreply.github.com>
2023-11-25 15:08:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // Database connection
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {});
