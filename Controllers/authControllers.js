a426c30e7308f69f8386c54910467fee23985919
unknown <16635847+Areeb2016@users.noreply.github.com>
2023-11-26 19:32:25 +0500

    Update authControllers.js

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -2,6 +2,7 @@ const User = require("../models/users");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
+
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -76,6 +77,7 @@ const resetPassword = async (req, res) => {
 
     // Find the user by email
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -88,22 +90,68 @@ const resetPassword = async (req, res) => {
     // Update user with reset token and expiry
     user.resetToken = resetToken;
     user.resetTokenExpiry = Date.now() + 3600000; // 1 hour
+
+    function generateRandomWord(length) {
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let randomWord = "";
+
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomWord += characters.charAt(randomIndex);
+      }
+
+      return randomWord;
+    }
+
+    const randomWord = generateRandomWord(6);
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(randomWord, 10);
+
+    // Update user's password with the hashed random password
+    user.password = hashedPassword;
+
     await user.save();
 
     // Send password reset email
     const transporter = nodemailer.createTransport({
       // configure nodemailer transporter (e.g., Gmail)
+      host: "smtp.imitate.email",
+      port: 587, // or 587 for TLS
+      secure: false, // use SSL
+      auth: {
+        user: "147ac2ea-8a9a-4370-8887-018c0bdbe7a4", // your Gmail email address
+        pass: "ce50500a-18b4-49b3-ba45-395a20995442", // the app-specific password generated in your Google Account
+      },
     });
-
+    console.log(email, "<");
     const mailOptions = {
       // configure mail options
+      from: '"Admin" psgamerlegend@gmail.com', // sender address
+      to: email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text:
+        "Forgot your password? \nNo worries we made a new one for you!\n" +
+        randomWord, // plain text body
     };
+    console.log(mailOptions, "<");
 
-    await transporter.sendMail(mailOptions);
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Email sent successfully");
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: "Error sending email: error", success: false });
+    }
 
-    res.json({ message: "Password reset email sent successfully" });
+    res.json({
+      message: "Password reset email sent successfully",
+      success: true,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error", success: false });
   }
 };
 
